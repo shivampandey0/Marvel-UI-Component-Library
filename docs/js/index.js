e331c16a6acfd72a62aa0420cbf9a87bc5507739
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-02-09 23:08:26 +0530

    feat: getting started page with mobile view

--- docs/js/index.js
+++ docs/js/index.js
@@ -9,3 +9,20 @@ hamburger.addEventListener('click', ()=> {
 closenav.addEventListener('click', ()=> {
     mobNav.style.width = null;
 });
+
+
+// Mobile Drawer
+let isMobile = false;
+const mobileMenu = document.querySelector(".mobile-menu");
+const drawer = document.querySelector(".drawer");
+
+mobileMenu.addEventListener('click',() => {
+    isMobile = !isMobile;
+    if (isMobile) {
+        drawer.classList.add("open-drawer");
+        mobileMenu.innerHTML = ` Menu <i class="fa-solid fa-angle-down"></i>`;
+    }else{
+        drawer.classList.remove("open-drawer");
+        mobileMenu.innerHTML = ` Menu <i class="fa-solid fa-angle-up"></i>`;
+    }
+})
