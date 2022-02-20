bb1c244f2807ebc6953ee73640c19280de222431
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-02-21 01:28:35 +0530

    [chore] code format

--- docs/js/snackbar.js
+++ docs/js/snackbar.js
@@ -1,4 +1,3 @@
-
 // Snackbars
 const leadingSnackbarBtn = document.getElementById("leading-snackbar-btn");
 const baselineSnackbarBtn = document.getElementById("baseline-snackbar-btn");
@@ -9,33 +8,37 @@ const leadingSnackbar = document.getElementById("leading-snackbar");
 const baselineSnackbar = document.getElementById("baseline-snackbar");
 const trailingSnackbar = document.getElementById("trailing-snackbar");
 
-baselineSnackbarBtn.addEventListener('click', () => showSnackbar(baselineSnackbar));
-leadingSnackbarBtn.addEventListener('click', () => showSnackbar(leadingSnackbar));
-trailingSnackbarBtn.addEventListener('click', () => showSnackbar(trailingSnackbar));
+baselineSnackbarBtn.addEventListener("click", () =>
+  showSnackbar(baselineSnackbar)
+);
+leadingSnackbarBtn.addEventListener("click", () =>
+  showSnackbar(leadingSnackbar)
+);
+trailingSnackbarBtn.addEventListener("click", () =>
+  showSnackbar(trailingSnackbar)
+);
 
 snackCloseBtn.forEach((closeBtn) => {
-    closeBtn.addEventListener("click", (e) => {
-        let parent = closeBtn.parentElement.parentElement.id;
-        console.log(parent.split('-')[0]);
-        switch (parent.split('-')[0]) {
-            case "baseline":
-                baselineSnackbar.style.display = "none";
-                break;
-            case "leading":
-                leadingSnackbar.style.display = "none";
-                break;
-            case "trailing":
-                trailingSnackbar.style.display = "none";
-                break;
-        }
-    });
+  closeBtn.addEventListener("click", (e) => {
+    let parent = closeBtn.parentElement.parentElement.id;
+    switch (parent.split("-")[0]) {
+      case "baseline":
+        baselineSnackbar.style.display = "none";
+        break;
+      case "leading":
+        leadingSnackbar.style.display = "none";
+        break;
+      case "trailing":
+        trailingSnackbar.style.display = "none";
+        break;
+    }
+  });
 });
 
+const showSnackbar = (id) => {
+  id.style.display = "flex";
 
-const showSnackbar = id => {
-    id.style.display = 'flex';
-
-    setTimeout(() => {
-        id.style.display = 'none';
-    }, 2000);
-}
+  setTimeout(() => {
+    id.style.display = "none";
+  }, 2000);
+};
